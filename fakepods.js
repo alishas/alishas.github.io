70877b1b64513647e6356df4b0919c569d52705b
alishasaxena <alisha_saxena@hotmail.com>
2014-10-03 12:42:51 -0400

    filter

--- fakepods.js
+++ fakepods.js
@@ -48,7 +48,9 @@ function handleResponse(responseText) {
 	var out = document.getElementById("out")
 	for(i=0;i<messages.length;i++){
 		var message = messages[i];
-		html= html+document.getElementById("podurl").value+"'s favorite food is "+message.food+" and favorite color is "+message.color+".<br>";
+		if(message.food&&message.color){
+			html= html+document.getElementById("podurl").value+"'s favorite food is "+message.food+" and favorite color is "+message.color+".<br>";
+		}
 	}
 	out.innerHTML=html;
 	
